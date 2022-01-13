d895980ccbf856fee7ee03bce5f7f119fb520ab8
Gabriel Ford <gabe@fordltc.net>
2022-01-13 10:41:24 -0500

    Worked on the Credentials page a big, also added social buttons

--- express_server.js
+++ express_server.js
@@ -4,6 +4,22 @@ const port = 3000
 
 app.use(express.static('static'));
 
+app.get('/contact', function(req, res) {
+    res.sendFile(__dirname + "/static/contact.html")
+})
+
+app.get('/credentials', function(req, res) {
+    res.sendFile(__dirname + "/static/credentials.html")
+})
+
+app.get('/work_experience', function(req, res) {
+    res.sendFile(__dirname + "/static/work_experience.html")
+})
+
+app.get('/projects', function(req, res) {
+    res.sendFile(__dirname + "/static/projects.html")
+})
+
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Example app listening at http://192.168.0.8:${port}`)
 })
